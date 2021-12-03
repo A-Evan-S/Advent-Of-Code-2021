ae2db367a52b17679a9827d8b45f69d59facb031
Eavn Schor <aevanschor@gmail.com>
2021-12-02 23:13:21 -0800

    Day 3

--- Day_03/day3.py
+++ Day_03/day3.py
@@ -1,17 +1,20 @@
 from aoc_utils import timed
 
 def main():
-    input = []
+    input = [0, []]
     with open('day3_input.txt') as f:
         for line in f:
-            input.append(int(line[::-1], 2))
+            input[0] = len(line.strip())
+            input[1].append(int(line[::-1], 2))
     print("Part 1:", timed(part1, input))
     print("Part 2:", timed(part2, input))
 
 def part1(input):
+    size, input = input
     gamma, epsilon = 0, 0
-    for i in range(12):
-        common = sum(x & 1 << i != 0 for x in input) > len(input) / 2
+    for _ in range(size):
+        common = 2 * sum(x % 2 for x in input) >= len(input)
+        input = [x // 2 for x in input]
         gamma = 2 * gamma + common
         epsilon = 2 * epsilon + (not common)
     return gamma * epsilon
@@ -20,8 +23,9 @@ def part2(input):
     return get_rating(input, True) * get_rating(input, False)
 
 def get_rating(input, get_common):
+    size, input = input
     result = 0
-    for i in range(12):
+    for _ in range(size):
         result *= 2
         common = 2 * sum(x % 2 for x in input) >= len(input)
         result += common ^ (not get_common and len(input) > 1)
