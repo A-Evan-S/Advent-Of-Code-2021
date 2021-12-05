e718ddd0a3739fd5d489926d68cef707258da82b
Eavn Schor <aevanschor@gmail.com>
2021-12-04 16:00:54 -0800

    Day 5 Prep

---
+++ Day_05/day5.py
@@ -0,0 +1,18 @@
+from aoc_utils import timed
+
+def main():
+    input = []
+    with open('day5_input.txt') as f:
+        for line in f:
+            input.append(line.strip())
+    print("Part 1:", timed(part1, input))
+    print("Part 2:", timed(part2, input))
+
+def part1(input):
+    return "Incomplete"
+
+def part2(input):
+    return "Incomplete"
+
+if __name__ == '__main__':
+    main()
