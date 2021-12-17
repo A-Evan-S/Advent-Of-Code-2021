3ebf7e3ed7ec24cb608f7dc71662057caf5bc168
Eavn Schor <aevanschor@gmail.com>
2021-12-16 20:34:45 -0800

    Improved parsing through binary by including `Code` object

--- Day_16/day16.py
+++ Day_16/day16.py
@@ -4,51 +4,52 @@ from aoc_utils import timed
 
 def main():
     with open('day16_input.txt') as f:
-        input = ''.join(f'{int(x, 16):04b}' for x in f.readline())
+        input = f.readline()
     print("Part 1:", timed(part1, input))
     print("Part 2:", timed(part2, input))
 
-def part1(code):
+def part1(input):
     global version_total
     version_total = 0
-    parse_packet(code, MutableInt())
+    parse_packet(Code(input))
     return version_total
 
-def part2(code):
-    return parse_packet(code, MutableInt())
+def part2(input):
+    return parse_packet(Code(input))
 
-class MutableInt:
-    def __init__(self, val=0):
-        self.val = val
+class Code:
+    def __init__(self, hexcode, index=0):
+        self.code = ''.join(f'{int(x, 16):04b}' for x in hexcode)
+        self.index = index
 
-def parse_bits(code, index, n):
-    val = int(code[index.val: index.val + n], 2)
-    index.val += n
-    return val
+    def parse_bits(self, n):
+        val = int(self.code[self.index: self.index + n], 2)
+        self.index += n
+        return val
 
-def parse_packet(code, index):
+def parse_packet(code):
     global version_total
-    version = parse_bits(code, index, 3)
+    version = code.parse_bits(3)
     version_total += version
-    id = parse_bits(code, index, 3)
+    id = code.parse_bits(3)
     if id == 4:  # Literal
         num = 0
-        while parse_bits(code, index, 1):
-            num = (num << 4) + parse_bits(code, index, 4)
-        num = (num << 4) + parse_bits(code, index, 4)
+        while code.parse_bits(1):
+            num = (num << 4) + code.parse_bits(4)
+        num = (num << 4) + code.parse_bits(4)
         return num
     else:  # Operator
         arguments = []
-        if parse_bits(code, index, 1):  # Sub Packets
-            sub_packet_count = parse_bits(code, index, 11)
+        if code.parse_bits(1):  # Sub Packets
+            sub_packet_count = code.parse_bits(11)
             for _ in range(sub_packet_count):
-                arg = parse_packet(code, index)
+                arg = parse_packet(code)
                 arguments.append(arg)
         else:  # Length
-            length = parse_bits(code, index, 15)
-            end_index = index.val + length
-            while index.val < end_index:
-                arg = parse_packet(code, index)
+            length = code.parse_bits(15)
+            end_index = code.index + length
+            while code.index < end_index:
+                arg = parse_packet(code)
                 arguments.append(arg)
 
         if id == 0:
